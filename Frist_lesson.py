6e21c5441d7bdb3d9b6788bff2d9e2dc30aef3ed
van-gogh <weiqifan0524@gmail.com>
2026-05-21 22:16:29 +0800

    day9.0

---
+++ Frist_lesson.py
@@ -0,0 +1,14 @@
+import sys
+import math
+print(sys.platform)
+print(2**100)
+x = 'Hack!'
+print(x*8)
+
+print(math.pi)
+print(math.sqrt(81))
+
+
+S = bytearray(b'deep')
+S.extend(b' learning')
+print(S.decode())
