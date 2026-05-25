fa21cf1d035851c48278a702e52ae8504016f4c6
van-gogh <weiqifan0524@gmail.com>
2026-05-25 21:59:47 +0800

    day11.0

--- Frist_lesson.py
+++ Frist_lesson.py
@@ -28,7 +28,7 @@ list_L = [1, 2, 3, 4]
 list_L = list_L + [5, 6, 7]  # or L.extend([5,6,7])
 print(list_L)
 
-D = {'name': 'Pat', 'job': 'dev', 'age':40}
+D = {'name': 'Pat', 'job': 'dev', 'age': 40}
 print(D['name'])
 
 C = {}
@@ -43,6 +43,41 @@ print(set_A & set_B, set_A | set_B)
 
 print(type(set_A))
 
-tuple_A = 1,2,3,4
+tuple_A = 1, 2, 3, 4
 print(tuple_A)
+
+# while True:
+# reply = input('Enter text:')
+# if reply == 'stop' : break
+# print(reply.upper())
+
+L = [1, 2, 3, 4]
+while L:
+    front, L = L[0], L[1:]
+    print(front, L)
+
+seq = [1, 2, 3, 4]
+a, *b = seq
+print(b)
+
+x = 10
+while x:
+    x -= 1
+    if x % 2 != 0: continue
+    print(x)
+
+#num0 = 1
+#while True:
+#    tool = input(f'{num0} What\'s your favorite language? please enter:')
+#    if tool == 'stop': break
+#    print('Bravo!' if tool == 'Python' else 'Try again...')
+#    num0 += 1
+
+#num1 = 1
+#while (tool := input(f'{num1}) What\'s your favorite language? ')) != 'stop':
+#    print('Bravo!' if tool == 'Python' else 'Try again...')
+#    num1 += 1
+
+for x in [1,2,3,4]:
+    print(x**2, end=' ')
 
