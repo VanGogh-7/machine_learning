e2a628419d8196e1fffe4ac4e24b3403fd408b69
van-gogh <weiqifan0524@gmail.com>
2026-05-29 18:48:25 +0800

    day13.0

--- Chapter_3.py
+++ Chapter_3.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import fetch_openml
 import matplotlib.pyplot as plt
-
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
 mnist = fetch_openml('mnist_784', as_frame = False)
 
 X, y = mnist.data, mnist.target
@@ -14,9 +15,23 @@ def plot_digit(image_data):
     plt.axis('off')
 
 some_digit = X[0]
-plot_digit(some_digit)
-plt.show()
+#plot_digit(some_digit)
+#plt.show()
 
 #print(y[0])
 
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[0:60000], y[60000:]
+
+# training a binary classifier
+
+y_train_5 = (y_train == '5')
+y_test_5 = (y_test == '5')
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+pred = sgd_clf.predict([some_digit])
+print(pred)
+
+cvs = cross_val_score(sgd_clf, X_train, y_train_5, cv = 3, scoring="accuracy")
+print(cvs)
--- Frist_lesson.py
+++ Frist_lesson.py
@@ -81,3 +81,17 @@ while x:
 for x in [1,2,3,4]:
     print(x**2, end=' ')
 
+def times0(x, y):
+    z = x * y
+    return z
+
+times1 = lambda x, y : x * y
+
+def intersect(seq1, seq2):
+    res = []
+    for x in seq1:
+        if x in seq2:
+            res.append(x)
+    return res
+
+
